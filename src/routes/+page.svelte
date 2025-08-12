6b6e6246f907ecc61b40a4df3cada4db0cabe0d5
gechternacht <gabriel@agenciavinci.com.br>
2025-08-12 03:02:33 -0300

    fabric replacer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,26 @@
 <script>
-  // @ts-nocheck
   import { Canvas, Image as FabricImage } from "fabric";
   import { onMount } from "svelte";
   import { listen } from "@tauri-apps/api/event";
   import { convertFileSrc } from "@tauri-apps/api/core";
+  import { open } from "@tauri-apps/plugin-dialog";
 
-  onMount(() => {
+  const pickopenfile = async () => {
+    const file = await open({
+      multiple: true,
+      directory: true,
+    });
+    return file;
+  };
+
+  onMount(async () => {
     const canvas = new Canvas("can", {
       selection: true,
       preserveObjectStacking: true,
+      selectionColor: "rgba(0, 120, 215, 0.3)",
+      selectionBorderColor: "rgba(0, 120, 215, 1)",
+      selectionLineWidth: 1,
+      selectionDashArray: [4, 2],
     });
 
     function resizeCanvas() {
@@ -20,44 +32,76 @@
     resizeCanvas();
     listen("tauri://drag-drop", (e) => {
       try {
-        let currentX = 100;
+        console.log(e);
+
+        const pointer = canvas.getPointer({
+          clientX: e.payload.position.x,
+          clientY: e.payload.position.y,
+        });
+
+        let currentX = pointer.x;
+        let currentY = pointer.y;
 
         const tryGrid = e.payload.paths?.length > 3;
         console.log("drag-drop event", e);
-        e.payload.paths.forEach((droppedEl, i) => {
-          const url = convertFileSrc(droppedEl);
+        // const maxWidth = canvas.getWidth() / canvas.getZoom();
+
+        for (let i = 0; i < e.payload.paths.length; i++) {
+          const url = convertFileSrc(e.payload.paths[i]);
           FabricImage.fromURL(url)
             .then((img) => {
+              if (i === 0) {
+                currentX = currentX - img.width / 2;
+                currentY = currentY - img.height / 2;
+              }
+              // if (img.width > maxWidth) {
+              //   const scale = maxWidth / img.width;
+              //   img.scaleX = scale;
+              //   img.scaleY = scale;
+              // }
+
               canvas.add(img);
               img.lockRotation = true;
               img.setControlsVisibility({ mtr: false });
               img.set({
                 left: currentX,
-                top: 100,
+                top: currentY,
                 originX: "left",
                 originY: "top",
               });
 
-              const maxWidth = canvas.width;
-              if (img.width > maxWidth) {
-                const scale = maxWidth / img.width;
-                img.scaleX = scale;
-                img.scaleY = scale;
-              }
-
               img.setCoords();
               canvas.renderAll();
-
+              canvas.setActiveObject(img);
               currentX += img.getScaledWidth();
             })
 
             .catch((err) => console.error(err));
-        });
+        }
       } catch (error) {
         console.warn(error);
       }
     });
 
+    window.addEventListener("keydown", function (e) {
+      if (e.ctrlKey && e.key === "s") {
+        e.preventDefault();
+        const allObjects = canvas.getObjects();
+        for (let i = 0; i < allObjects.length; i++) {
+          console.log(allObjects[i]._element.src);
+        }
+      }
+    });
+
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Delete" || e.key === "Backspace") {
+        const selectedObjects = canvas.getActiveObjects();
+        console.log(selectedObjects); //
+        for (let i = 0; i < selectedObjects.length; i++) {
+          canvas.remove(selectedObjects[i]);
+        }
+      }
+    });
     canvas.on("mouse:wheel", function (opt) {
       const delta = opt.e.deltaY;
       let zoom = canvas.getZoom();
@@ -69,28 +113,31 @@
       opt.e.preventDefault();
       opt.e.stopPropagation();
     });
-    let isPanning = false;
+    let isMoving = false;
     let lastPosX = 0;
     let lastPosY = 0;
+
     window.addEventListener("mousedown", (e) => {
+      //canvas.on("mouse:down", (opt) => {
       if (e.button === 1) {
         console.log("middle");
-        isPanning = true;
+        isMoving = true;
         lastPosX = e.clientX;
         lastPosY = e.clientY;
         canvas.selection = false;
       }
     });
     window.addEventListener("mouseup", (e) => {
+      //canvas.on("mouse:up", (opt) => {
       if (e.button === 1) {
         console.log("middle");
-        isPanning = false;
+        isMoving = false;
         canvas.selection = true;
       }
     });
 
     canvas.on("mouse:move", function (opt) {
-      if (isPanning) {
+      if (isMoving) {
         const e = opt.e;
         const deltaX = e.clientX - lastPosX;
         const deltaY = e.clientY - lastPosY;
@@ -99,18 +146,21 @@
         canvas.relativePan({ x: deltaX, y: deltaY });
       }
     });
+
     //onmount
   });
 </script>
 
 <main class="container">
-  <div class="title-thing">dnd image collection</div>
-  <canvas id="can" width="1200" height="800"></canvas>
+  <div class="title-thing">
+    dnd image collection <button on:click={pickopenfile}>open</button>
+  </div>
+  <canvas id="can"></canvas>
 </main>
 
 <style>
   .container {
-    background: transparent;
+    background-color: rgba(255, 255, 255, 0.014);
   }
   .title-thing {
     font-family: "MingLiU";
@@ -122,6 +172,7 @@
     left: 0;
     font-size: 11px;
     letter-spacing: 3px;
+    z-index: 2;
   }
 
   canvas {
